5384560cc12a709df2805126c80791ca1bc326a3
hex benjamin <hex@hexbenjam.in>
2024-04-25 20:29:49 -0400

    v0.1.2 - add attribute passthrough to Container superclass

--- flet_iconoir/iconoir_icon.py
+++ flet_iconoir/iconoir_icon.py
@@ -1,4 +1,4 @@
-from typing import Literal
+from typing import Any, Literal
 
 import flet as ft
 
@@ -30,3 +30,27 @@ class IconoirIcon(ft.Container):
 
         self.width = self.icon_size
         self.height = self.icon_size
+
+    def __setattr__(self, name: str, value: Any) -> None:
+        if name == "icon_color":
+            self.icon_color = value
+        elif name == "icon_name":
+            self.icon_name = value
+        elif name == "icon_set":
+            self.icon_set = value
+        elif name == "icon_size":
+            self.icon_size = value
+
+        return super().__setattr__(name, value)
+
+    def __getattribute__(self, name: str) -> Any:
+        if name == "icon_name":
+            return self.icon_name
+        if name == "icon_set":
+            return self.icon_set
+        if name == "icon_size":
+            return self.icon_size
+        if name == "icon_color":
+            return self.icon_color
+
+        return super().__getattribute__(name)
